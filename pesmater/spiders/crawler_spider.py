ead50178d77cd8a873a3d6582502d43668d51931
FiDai <hoangphidai123@gmail.com>
2021-05-08 22:15:52 +0700

    optimize

--- pesmater/spiders/crawler_spider.py
+++ pesmater/spiders/crawler_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 from scrapy import Spider
 from scrapy.selector import Selector
+from scrapy.loader import ItemLoader
+
 from pesmater.items import PesmaterItem
 from urllib.parse import urljoin
 import json
@@ -24,9 +26,11 @@ class CrawlerSpider(Spider):
             yield scrapy.Request(url=urljoin(response.url, url), callback=self.parse_detail,
                                  meta={'avatar': avatar, 'url': urljoin(response.url, url)})
 
-        # if self.page <= 859: self.page += 1 url =
-        # 'https://www.pesmaster.com/pes-2021/search/search.php?myclub=yes&sort=ovr&sort_order=desc&page=' + str(
-        # self.page) yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
+        if self.page <= 10:
+            self.page += 1
+            url = 'https://www.pesmaster.com/pes-2021/search/search.php?myclub=yes&sort=ovr&sort_order=desc&page=' + str(
+                self.page)
+        yield scrapy.Request(url=url, callback=self.parse, dont_filter=True)
 
     def parse_detail(self, response):
         detail = response.xpath('//*')
@@ -42,33 +46,15 @@ class CrawlerSpider(Spider):
         item['player_max_level'] = detail.xpath('div[@class="level-slider-container"]/input/@max').get()
         item['url'] = response.meta['url']
 
-        #
-        player_details = detail.xpath('table[@class="player-info"]/tbody/tr')
-        if len(player_details) == 11:
-            item['fullName'] = player_details[0].xpath('td/text()').extract()[1]
-            item['nationality'] = player_details[1].xpath('td/text()').extract()[1]
-            item['team'] = player_details[2].xpath('td/text()').extract()[1]
-            item['strongerFoot'] = player_details[5].xpath('td/text()').extract()[1]
-            item['height'] = player_details[6].xpath('td/text()').extract()[1]
-            item['weight'] = player_details[7].xpath('td/text()').extract()[1]
-            item['type'] = player_details[len(player_details) - 1].xpath('td/a/text()').get()
-        elif len(player_details) == 10:
-            item['nationality'] = player_details[0].xpath('td/text()').extract()[1]
-            item['team'] = player_details[1].xpath('td/text()').extract()[1]
-            item['strongerFoot'] = player_details[4].xpath('td/text()').extract()[1]
-            item['height'] = player_details[5].xpath('td/text()').extract()[1]
-            item['weight'] = player_details[6].xpath('td/text()').extract()[1]
-            item['type'] = player_details[len(player_details) - 1].xpath('td/a/text()').get()
-        else:
-            item['fullName'] = player_details[0].xpath('td/text()').extract()[1]
-            item['nationality'] = player_details[1].xpath('td/text()').extract()[1]
-            item['market_value'] = player_details[2].xpath('td/text()').extract()[1]
-            item['team'] = player_details[3].xpath('td/text()').extract()[1]
-            item['contractDur'] = player_details[4].xpath('td/text()').extract()[1]
-            item['strongerFoot'] = player_details[7].xpath('td/text()').extract()[1]
-            item['height'] = player_details[8].xpath('td/text()').extract()[1]
-            item['weight'] = player_details[9].xpath('td/text()').extract()[1]
-            item['type'] = player_details[len(player_details) - 1].xpath('td/a/text()').get()
+        ds = detail.xpath('table[@class="player-info"]/tbody/tr')
+
+        for i, item_detail in enumerate(ds):
+            name = str(item_detail.xpath('td/text()').extract()[0]).lower().replace(' ', '_')
+            item.fields[name] = scrapy.Field()
+            if i == len(ds) - 1:
+                item[name] = item_detail.xpath('td/a/text()').get()
+            else:
+                item[name] = str(item_detail.xpath('td/text()').extract()[1])[1:]
 
         item['player_stats'] = json.loads(str(detail.xpath('script/text()').re('.*levelStats.*'))[25:][:-3])
 
